cb14dbab6e4764905dc3cb43971cdd06c8b4880b
johusha <johusha@icloud.com>
2022-04-11 10:02:29 -0600

    ✨ Made the sequence clip button work

--- apps/rebase.tv/src/index.js
+++ apps/rebase.tv/src/index.js
@@ -1,16 +1,56 @@
-const videoElems = document.getElementsByTagName('video')
+// const videoElems = document.getElementsByTagName('video')
 
-for (var elem of videoElems) {
-  const parent = elem.parent
-  const slider = parent.getElementsByTagName('input')
+// for (var elem of videoElems) {
+//   const parent = elem.parent
+//   const slider = parent.getElementsByTagName('input')
 
 
-  const video = elem
-  slider.addEventListener('something', event => {
-    // set playback speed of the preview
-    const value = event.value
+//   const video = elem
+//   slider.addEventListener('something', event => {
+//     // set playback speed of the preview
+//     const value = event.value
 
-    video.playbackSpeed = value
-    console.log('something changed to ', value)
+//     video.playbackSpeed = value
+//     console.log('something changed to ', value)
+//   })
+// }
+
+window.addEventListener('load', () => {
+  let button = getSequenceButton()
+  console.log(button)
+
+  button.addEventListener('click', () => {
+    const videos = document.getElementsByTagName('video')
+
+    playVideosInSequence(videos)
   })
+})
+
+function getSequenceButton() {
+  const button = document.querySelector("#clips-controls > input[type=button]:nth-child(1)")
+
+  return button
+}
+
+function playAllVideos() {
+  for (var video of document.getElementsByTagName('video')) {
+    video.play()
+  }
+}
+
+function playVideosInSequence(videos) {
+  for (var video of videos) {
+    video.pause()
+    video.currentTime = 0
+
+    video.addEventListener('ended', event => {
+      const sibling = event.target.parentElement.nextElementSibling.firstElementChild
+      if (sibling == undefined)
+        return console.warn('no next clip to play')
+
+      sibling.play()
+    })
+  }
+
+  videos[0].play()
 }
